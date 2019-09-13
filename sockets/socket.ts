8a88a353a792cb1fd0a78009d484f90d35053833
Axel Barria <axel.barria@gmail.com>
2019-09-13 10:50:12 -0300

    seccion 4

---
+++ sockets/socket.ts
@@ -0,0 +1,30 @@
+import { Socket } from 'socket.io';
+import socketIO from 'socket.io';
+
+
+
+export const desconectar = (cliente: Socket) =>
+
+    cliente.on('disconnect', () => {
+        console.log('Cliente DESCONECTADO');
+    });
+
+
+}
+
+
+//Escuchar mensajes
+export const mensaje = (cliente: Socket, io: socketIO.Server) =>
+
+cliente.on('mensaje', ( payload: { de: string, cuerpo: string } ) => {
+
+    console.log('mensaje recibido', payload);
+
+    io.emit( 'mensaje-nuevo', payload );
+
+
+});
+    
+
+
+}
